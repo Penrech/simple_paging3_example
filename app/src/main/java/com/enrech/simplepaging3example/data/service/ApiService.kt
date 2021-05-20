bc8353bebe400cc3995b29ff7e83ddd6df7589ff
pau.enrech <pau.enrech@promocionesfarma.com>
2021-05-20 17:55:13 +0200

    paging3 working

--- app/src/main/java/com/enrech/simplepaging3example/data/service/ApiService.kt
+++ app/src/main/java/com/enrech/simplepaging3example/data/service/ApiService.kt
@@ -1,11 +1,14 @@
 package com.enrech.simplepaging3example.data.service
 
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiService {
     fun instantiate(): GithubApi =
         Retrofit.Builder()
             .baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(GithubApi::class.java)
+
 }
--- app/src/main/java/com/enrech/simplepaging3example/model/Repo.kt
+++ app/src/main/java/com/enrech/simplepaging3example/model/Repo.kt
@@ -1,3 +1,5 @@
 package com.enrech.simplepaging3example.model
 
-data class Repo(val fullName: String)
+import com.google.gson.annotations.SerializedName
+
+data class Repo(@SerializedName("full_name") val fullName: String)
