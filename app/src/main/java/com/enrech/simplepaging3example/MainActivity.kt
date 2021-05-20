917c9fe6e9e3ac382307d6f6a60cff90c7be10b0
pau.enrech <pau.enrech@promocionesfarma.com>
2021-05-20 16:01:06 +0200

    basic ui created

--- app/src/main/java/com/enrech/simplepaging3example/MainActivity.kt
+++ app/src/main/java/com/enrech/simplepaging3example/MainActivity.kt
@@ -2,10 +2,29 @@ package com.enrech.simplepaging3example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.enrech.simplepaging3example.view.adapter.ReposAdapter
+import com.enrech.simplepaging3example.view.viewmodel.GithubViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: GithubViewModel by viewModels()
+    private var searchJob: Job? = null
+    private val adapter: ReposAdapter = ReposAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun search(username: String) {
+        searchJob?.cancel()
+        searchJob = lifecycleScope.launch {
+            viewModel.searchRepos(username).collect { adapter.submitData(it) }
+        }
     }
 }
---
+++ app/src/main/java/com/enrech/simplepaging3example/view/custom/SearchView.kt
@@ -0,0 +1,53 @@
+package com.enrech.simplepaging3example.view.custom
+
+import android.content.Context
+import android.content.Context.INPUT_METHOD_SERVICE
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.FrameLayout
+import com.enrech.simplepaging3example.databinding.SearchViewBinding
+
+class SearchView(context: Context, attrs: AttributeSet): FrameLayout(context, attrs) {
+
+    private val binding: SearchViewBinding =
+        SearchViewBinding.inflate(
+            LayoutInflater.from(context),
+            this,
+            false
+        )
+
+    private lateinit var onSearchAction: (query: String) -> Unit
+
+    init {
+        with(binding) {
+            this@SearchView.addView(root)
+
+            searchButton.setOnClickListener {
+                if (::onSearchAction.isInitialized) {
+                    onSearchAction(binding.searchEt.toString())
+                    closeKeyboard(it)
+                }
+            }
+
+            searchTextInput.setEndIconOnClickListener {
+                searchEt.text = null
+                searchTextInput.setEndIconActivated(false)
+                closeKeyboard(it)
+            }
+        }
+
+    }
+
+    fun addOnSearchListener(callback: (query: String) -> Unit) {
+        onSearchAction = callback
+    }
+
+    private fun closeKeyboard(view: View) {
+        (context?.getSystemService(INPUT_METHOD_SERVICE) as? InputMethodManager)?.apply {
+            hideSoftInputFromWindow(view.applicationWindowToken, 0)
+        }
+    }
+
+}
