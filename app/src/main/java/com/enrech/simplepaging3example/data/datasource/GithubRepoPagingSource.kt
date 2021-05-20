e35df42c507b17b2a855734db28b321c3f8c0c91
pau.enrech <pau.enrech@promocionesfarma.com>
2021-05-20 09:31:07 +0200

    create datasource

---
+++ app/src/main/java/com/enrech/simplepaging3example/data/datasource/GithubRepoPagingSource.kt
@@ -0,0 +1,35 @@
+package com.enrech.simplepaging3example.data.datasource
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.enrech.simplepaging3example.data.service.GitHubApiService
+import com.enrech.simplepaging3example.model.Repo
+
+private const val INITIAL_PAGE = 1
+
+class GithubRepoPagingSource(
+    private val api: GitHubApiService,
+    private val username: String
+) : PagingSource<Int, Repo>() {
+
+    override fun getRefreshKey(state: PagingState<Int, Repo>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
+        }
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Repo> {
+        return try {
+            val page = params.key ?: INITIAL_PAGE
+            val response = api.fetchRepos(username, page, params.loadSize)
+            LoadResult.Page(
+                data = response,
+                prevKey = if (page == INITIAL_PAGE) null else page - 1,
+                nextKey = if (response.isEmpty()) null else page + 1
+            )
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+}
